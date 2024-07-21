05ab227a7e17668737d6f6f16c8371f1792979b3
dora <bigpowerpig@gmail.com>
2024-07-21 23:34:10 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <RouterView />
+
+  
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  font-family: "Microsoft JhengHei", "Heiti TC", "sans-serif";
+}
+img {
+  display: block;
+}
+html,
+body {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HeaderBox from '@/components/HeaderBox.vue'
+import ArticleBox from '@/components/ArticleBox.vue'
+import AsideBox from '@/components/AsideBox.vue'
+import mainBox from '@/components/mainBox.vue'
+import footerBox from '@/components/footerBox.vue'
+</script>
+
+<template>
+  <main>
+    <HeaderBox />
+    <ArticleBox />
+    <AsideBox />
+    <mainBox />
+    <footerBox />
+  </main>
+</template>
+
